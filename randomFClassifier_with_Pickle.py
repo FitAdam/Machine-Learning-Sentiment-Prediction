9f643f1070f974c1819804a88dda3658cf15827e
FitAdam <adam.george617@gmail.com>
2021-03-08 19:08:58 +0000

    refactoring

--- randomFClassifier_with_Pickle.py
+++ randomFClassifier_with_Pickle.py
@@ -9,14 +9,15 @@ from sklearn.metrics import classification_report, confusion_matrix, accuracy_sc
 
 
 """Read the data"""
-df = pd.read_csv(r'C:\Users\atuta\Documents\Projects\Stocks Machine Learning\Predictions\Combined_News_DJIA.csv', encoding = 'ISO-8859-1')
-df_test = pd.read_csv(r'C:\Users\atuta\Documents\Projects\Stocks Machine Learning\Predictions\3titles.csv', encoding = 'ISO-8859-1')
+df = pd.read_csv(r'C:\Users\atuta\Documents\Projects\Stocks Machine Learning\Combined_News_DJIA.csv', encoding = 'ISO-8859-1')
+df_test = pd.read_csv(r'C:\Users\atuta\Documents\Projects\Stocks Machine Learning\3titles.csv', encoding = 'ISO-8859-1')
 
 """Divide the data into sets"""
 
 #divide in train and test
-train = df
-test = df_test
+# Prepare the groups for tests. 
+train = df #df[df['Date']<'20150101']
+test = df_test #df[df['Date'] > '20141231']
 
 """Prepering data"""
 
@@ -56,12 +57,9 @@ trainDataset = countVector.fit_transform(headlines)
 
 testTransform =[]
 for row in range(0, len(test.index)):
-    print(f"This is row: {row}")
     testTransform.append(' '.join(str(x) for x in test.iloc[row, 0:3]))
     
 test_dataset = countVector.transform(testTransform)
-
-print(f"This is test_dataset: {test_dataset}")
 
 """Open pickle"""
 pickle_open = open("stocks_news_model.pickle", "rb")
@@ -73,8 +71,6 @@ randomForestClassifier = pickle.load(pickle_open)
 prediction = randomForestClassifier.predict(test_dataset)
 
 """ Print the result """
-print(prediction)
-prediction = (prediction > 0.59)
 print(prediction) # if true the stocks will go up, otherwise down
 
 
